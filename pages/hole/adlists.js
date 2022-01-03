19e6e0df6bb96082a0a1c45d11f2a7cc628e19d4
Fahad Hossain <set.name.fahad@gmail.com>
2022-01-03 19:54:21 +0100

    fix: remove full oisd list in favor of basic

--- pages/hole/adlists.js
+++ pages/hole/adlists.js
@@ -10,7 +10,7 @@ const AGGR_ADLISTS_URL = 'https://v.firebog.net/hosts/lists.php?type=tick';
 
 const INDIVIDUAL_ADLISTS_URLS = [
   'https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt',
-  'https://dbl.oisd.nl/',
+  'https://dbl.oisd.nl/basic/',
 ];
 
 export async function getServerSideProps({ res }) {
